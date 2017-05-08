2cfdef03eb7f535ab15a8e722c5eb2fcd42dd9d5
Clayton Barrows <cbarrows@login3.hpc.nrel.gov>
2017-05-08 10:46:22 -0600

    updating the split command

--- Workshop-Explorations/TimeDomain_Decomp/submitPLEXOS_split_execution.sh
+++ Workshop-Explorations/TimeDomain_Decomp/submitPLEXOS_split_execution.sh
@@ -6,7 +6,7 @@
 # No promises on all modules being correct and up to date or scripts working 
 
 ############################ USER MODIFIED SECTION ########################
-export p=052 #partitions
+export p=004 #partitions
 export o=002 #overlap days
 export feature="nodes=1,walltime=15:00"
 export alloc="PLEXOSMODEL"
@@ -38,7 +38,7 @@ echo "splitmodel.py -n $name.xml -m $model -p $p -o $o"
 #setsolverdefaults.py -n OLd$i/RMPP_Core_DB_Storage_6208.xml -m $1 -s CPLEX
 #execute splitmodel script from the phelper module
 #run splitmodel.py -h to get help on this function
-splitmodel.py -n $name.xml -m $model -p $p -o $o
+#splitmodel.py -n $name.xml -m $model -p $p -o $o
 
 #for every partition, do:
 for I in $(seq 001 $p); do
@@ -57,8 +57,10 @@ for I in $(seq 001 $p); do
  pwd
  #set the root directory for the to-be-submitted job
  PBS_O_WORKDIR=$(pwd)
- echo "qsub -A ${alloc} -q ${queue}  -l ${feature} -v filename="${name}",model="${model}_${p}P_OLd${o}_$i" ${runscript}"
+ submitcommand="qsub -A ${alloc} -q ${queue}  -l ${feature} -v filename="${name}",model="${model}_${p}P_OLd${o}_$i" ${runscript}"
  #submit job to HPC scheduler
+ echo $submitcommand
+ $submitcommand
  #qsub -A ${alloc} -q ${queue}  -l ${feature} -v filename="${name}",model="${model}_${p}P_OLd${o}_$i" ${runscript}
  #go back to root directory and start over
  cd $rootdir
