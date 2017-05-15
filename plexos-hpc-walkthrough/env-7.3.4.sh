4483447487f5a09ff119b738dbb07729ba7b53d5
Wesley Jones <wesley.jones@nrel.gov>
2017-05-15 10:11:25 -0600

    not using 7.3.4 in workshop

--- plexos-hpc-walkthrough/env-7.3.4.sh
+++
@@ -1,6 +0,0 @@
-module use /nopt/nrel/apps/modules/candidate/modulefiles
-module purge
-module load epel gcc mono/4.6.2.7 xpressmp/7.8.0 plexos/7.300.4
-export PLEXOS_TEMP=/scratch/$USER/tmp/$PBS_JOBID
-export TEMP=$PLEXOS_TEMP
-mkdir -p $PLEXOS_TEMP
