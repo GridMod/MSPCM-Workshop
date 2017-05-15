f488dc7b9c59c1f412c2061515bcead6be7d17e9
bstoll <brady.stoll@nrel.gov>
2017-05-15 14:10:38 -0600

    Updated plotting and added boxplots

--- Workshop-Explorations/Ramping-Analysis/R_ramping_analysis.R
+++ Workshop-Explorations/Ramping-Analysis/R_ramping_analysis.R
@@ -1,4 +1,6 @@
-# Copy interval generation for ramping analysis
+#--------------------------------------------------------------------------------------
+# Set up ramp data
+#--------------------------------------------------------------------------------------
 
 if(!'Type' %in% names(interval.generation)){
   interval.generation[, Type:=gen.type.mapping[name]]
@@ -13,7 +15,9 @@ interval.ramp.data = merge(interval.generation[,.(scenario, name, Type, generati
 interval.ramp.data[, day:=as.POSIXlt(time)[[8]]]
 
 
+#--------------------------------------------------------------------------------------
 # Plotting functions
+#--------------------------------------------------------------------------------------
 ramp_plot <- function(df,facet_scen=TRUE,facet_cols=3) {
   
   if (facet_scen){
@@ -28,12 +32,35 @@ ramp_plot <- function(df,facet_scen=TRUE,facet_cols=3) {
   return(p)
 }
 
+ramp_boxplot <- function(df){
+  p <- ggplot(df,aes(Type,ramp,color=scenario)) + geom_boxplot() +
+    scale_color_brewer(palette='Set1') +
+    xlab("") + ylab("Ramp") +
+    theme( legend.key =       element_rect(color = "grey80", size = 0.4),
+           legend.key.size =  grid::unit(0.9, "lines"),
+           legend.text =      element_text(size=text.plot/1.1,face = 'bold'),
+           axis.text =        element_text(size=text.plot/1.2,face = 'bold'),
+           axis.text.x =      element_text(hjust = 0, angle=-35),
+           axis.title =       element_text(size=text.plot, face='bold'),
+           axis.title.x =     element_text(vjust=-0.3),
+           strip.text       = element_text(size = text.plot,face = 'bold'),
+           panel.grid.major = element_line(colour = "grey85"),
+           panel.grid.minor = element_line(colour = "grey93"),
+           aspect.ratio =     0.5,
+           panel.spacing =     unit(1.0, "lines") )
+  return(p)
+}
 
+
+#--------------------------------------------------------------------------------------
+# Ramping calcuations and plotting
+#--------------------------------------------------------------------------------------
 # Calculate ramping as the range by day
 ramp.day.gen <- interval.ramp.data[, .(ramp=(max(generation)-min(generation))/max(capacity), capacity=max(capacity)), 
                                    by=.(scenario,name,Type,day)]
 ramp.day <- ramp.day.gen[, .(ramp=sum(ramp*capacity)/sum(capacity)), by=.(scenario,Type,day)]
-ramp_plot(ramp.day,FALSE) + ylab('Ramp, Fraction of Capacicty')
+ramp_plot(ramp.day,FALSE,4) + ylab('Ramp, Fraction of Capacicty')
+ramp_boxplot(ramp.day) + ylab('Ramp, Fraction of Capacicty')
 
 
 # Calculate ramping as the max change in an hour
